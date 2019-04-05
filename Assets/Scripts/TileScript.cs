873990d13676da12d579e4ccb2b9edb01201a9e1
Dombrewski <38660479+Dombrewski@users.noreply.github.com>
2019-04-05 18:41:04 +1100

    Game detects the tile the player clicks on
    Still need to figure out how to get the actual sprite associated with the tile.

---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileScript : TileBase {
+
+    public void GetTileData(Vector3Int position, Tilemaps.ITilemap tilemap, ref Tilemaps.TileData tileData)
+    {
+
+    }
+}
