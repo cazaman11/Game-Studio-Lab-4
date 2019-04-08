4156f6634698ca9b9807da084836be7510ca0837
cazaman11 <38904836+cazaman11@users.noreply.github.com>
2019-04-08 10:19:47 +1000

    Added sfx for goal and improved mute

---
+++ Assets/Scripts/GoalSoundScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalSoundScript : MonoBehaviour {
+    AudioSource winSfx;
+
+	// Use this for initialization
+	void Start () {
+        winSfx = GameObject.Find("testManager").GetComponent<AudioSource>();
+	}
+    /*
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("Player trigger");
+            winSfx.Play();
+        }
+        Debug.Log("Colliding");
+    }
+    */
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Debug.Log("Player trigger");
+            winSfx.Play();
+        }
+    }
+}
