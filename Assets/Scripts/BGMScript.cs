f4935452699024395befcacea60b9d4879eaa107
AlexanderLeydon <37314497+AlexanderLeydon@users.noreply.github.com>
2019-04-09 12:01:56 +1000

    Made BGM script

---
+++ Assets/Scripts/BGMScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMScript : MonoBehaviour {
+
+    [SerializeField]
+    AudioSource audio;
+    [SerializeField]
+    AudioClip ac1;
+    [SerializeField]
+    AudioClip ac2;
+
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+        audio.clip = ac1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!audio.isPlaying) {
+            audio.clip = ac2;
+            audio.loop = true;
+            audio.Play();
+        }
+	}
+}
