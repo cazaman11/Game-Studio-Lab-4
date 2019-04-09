ab80b257ea0f1ba23578f0e04265eb08d23addb8
AlexanderLeydon <37314497+AlexanderLeydon@users.noreply.github.com>
2019-04-09 11:39:13 +1000

    Transmog gun now has range restricted to be around the player

--- Assets/Scripts/TransmogScript.cs
+++ Assets/Scripts/TransmogScript.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TransmogScript : MonoBehaviour {
+public class TransmogScript : MonoBehaviour
+{
 
     Vector2 mousePos;
     [SerializeField]
@@ -11,42 +12,83 @@ public class TransmogScript : MonoBehaviour {
     GameObject rock;
     [SerializeField]
     GameObject background;
+    [SerializeField]
+    float tileSize = 0.32f;
 
     enum GunState { Quicksand, Rock };
     GunState currState = GunState.Rock;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         currState = GunState.Rock;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.E)) {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
             if (currState == GunState.Rock) currState = GunState.Quicksand;
             else currState = GunState.Rock;
         }
-        if (Input.GetMouseButtonDown(1)) {
+        if (Input.GetMouseButtonDown(1))
+        {
             if (currState == GunState.Rock) SwitchBlock(rock);
             else SwitchBlock(quicksand);
         }
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0))
+        {
             SwitchBlock(background);
         }
-	}
+    }
 
-    void SwitchBlock(GameObject pref) {
+    void SwitchBlock(GameObject pref)
+    {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 mousePos3D = new Vector3(mousePos.x, mousePos.y, -1f);
-        RaycastHit hit;
-        Physics.Raycast(mousePos3D, Vector3.forward, out hit, 1);
-        if (hit.collider)
+        if (InRange(mousePos3D))
         {
-            if (hit.collider.tag == "Dirt") {
-                Vector3 tempPos = hit.collider.transform.position;
-                Destroy(hit.collider.gameObject);
-                Instantiate(pref, tempPos, Quaternion.identity);
+            RaycastHit hit;
+            Physics.Raycast(mousePos3D, Vector3.forward, out hit, 1);
+            if (hit.collider)
+            {
+                if (hit.collider.tag == "Dirt")
+                {
+                    Vector3 tempPos = hit.collider.transform.position;
+                    Destroy(hit.collider.gameObject);
+                    if (pref.tag != "Background")
+                    {
+                        Instantiate(pref, tempPos, Quaternion.identity);
+                    }
+                    Instantiate(background, tempPos, Quaternion.identity);
+                }
             }
         }
+    }
+
+    bool InRange(Vector3 pos)
+    {
+        float upperXBounds = transform.position.x + tileSize;
+        float lowerXBounds = transform.position.x - tileSize;
+        float upperYBounds = transform.position.y + tileSize;
+        float lowerYBounds = transform.position.y - tileSize;
+        if (pos.x > upperXBounds)
+        {
+            return false;
+        }
+        if (pos.x < lowerXBounds)
+        {
+            return false;
+        }
+        if (pos.y > upperYBounds)
+        {
+            return false;
+        }
+        if (pos.y < lowerYBounds)
+        {
+            return false;
+        }
+        return true;
     }
 }
